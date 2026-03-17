3a11eb5cdfb0aeeb92d9e3d480c16b6148cd589b
Ecra90 <sharleymuthoni2@gmail.com>
2026-03-17 16:47:31 +0300

    react

--- src/components/GetProducts.jsx
+++ src/components/GetProducts.jsx
@@ -3,43 +3,48 @@ import { useEffect, useState } from "react";
 // useEffect==used to run side effects
 // to avoid refreshing the browser manually
 // puting empty dependencies===when we always need a new category to make sure it runs once
-const GetProducts=()=>{
-    const [products,setProducts]=useState("")
-    const [error,setError]=useState("")
-    const [loading,setLoading]=useState("")
-    const getproducts=async() => {
+const GetProducts = () => {
+    const [products, setProducts] = useState("")
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState("")
+    // path for our image
+    const img_url = "https://malombeswala.alwaysdata.net/static/images/"
+    const getproducts = async () => {
         setLoading("please wait.....")
-        try{
+        try {
             // connect to backend api
-            const response=await axios.get("https://malombeswala.alwaysdata.net/api/get_product_details")
+            const response = await axios.get("https://malombeswala.alwaysdata.net/api/get_product_details")
             // update the products hook with the data from the api
             setProducts(response.data)
             // after getting the response reset the loading hook
             setLoading("")
-        }catch(error){
+        } catch (error) {
             setLoading("")
             setError("something went wrong")
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         getproducts()
 
- },[])//empty dependency to ensure that the get products runs once with the components mounts
-    return(
+    }, [])//empty dependency to ensure that the get products runs once with the components mounts
+    return (
         <div className="row">
-                <h1 className="text-center"><b>Available products here</b></h1>
-                {loading}
-                {error}
+            <h1 className="text-center"><b>Available products here</b></h1>
+            {loading}
+            {error}
+            {products.map((product) => (
+
                 <div className="col-md-3 text-center mb-4">
-                {/*card with dummy data*/}
-                <div className="card shadow">
-                    <img src="" alt="" className="product_img mt-4"/>
-                    <h5 className="mt-2">Heading</h5>
-                    <p className="text-muted">Description</p>
-                    <b className="text-warning">cost KES</b>
-                    <button className="text-light btn-btn-light w-100 mt-2 bg-dark h-400" >Purchase now</button>
+                    {/*card with dummy data*/}
+                    <div className="card shadow">
+                        <img src={img_url + product.product_photo} alt="" className="product_img mt-4" />
+                        <h5 className="mt-2">Heading</h5>
+                        <p className="text-muted">Description</p>
+                        <b className="text-warning">cost KES</b>
+                        <button className="text-light btn-btn-light w-100 mt-2 bg-dark h-400" >Purchase now</button>
+                    </div>
                 </div>
-                </div>
+            ))}
         </div>
     )
 }
